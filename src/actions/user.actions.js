ac7bb7b37662a03b76f2655a102a13b24ace8b69
aaku14n <akarshgdps@gmail.com>
2017-12-25 02:25:58 +0530

    Ading cahges

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -6,6 +6,12 @@ export const USER_LOGIN_FAILURE = "user_login_failure";
 export const GET_AGENTS_REQUEST = "GET_AGENTS_request";
 export const GET_AGENTS_SUCCESS = "GET_AGENTS_success";
 export const GET_AGENTS_FAILURE = "GET_AGENTS_failure";
+export const GET_PAYMENT_REQUEST = "GET_PAYMENT_request";
+export const GET_PAYMENT_SUCCESS = "GET_PAYMENT_success";
+export const GET_PAYMENT_FAILURE = "GET_PAYMENT_failure";
+export const GET_MESSAGE_REQUEST = "GET_MESSAGE_request";
+export const GET_MESSAGE_SUCCESS = "GET_MESSAGE_success";
+export const GET_MESSAGE_FAILURE = "GET_MESSAGE_failure";
 
 export function userLoginRequest() {
   return {
@@ -89,6 +95,86 @@ export function getAgents(userObj) {
       );
     } catch (e) {
       dispatch(getAgentsFailure("Error in logging in user"));
+    }
+  };
+}
+
+export function getPaymentRequest() {
+  return {
+    type: GET_PAYMENT_REQUEST,
+    status: REQUEST
+  };
+}
+export function getPaymentSuccess(payments) {
+  return {
+    type: GET_PAYMENT_SUCCESS,
+    status: SUCCESS,
+    payments
+  };
+}
+export function getPaymentFailure(error) {
+  return {
+    type: GET_PAYMENT_FAILURE,
+    status: FAILURE,
+    error
+  };
+}
+export function getPayment() {
+  return async (dispatch, state) => {
+    dispatch(getPaymentRequest());
+    try {
+      let ref = await fire.database().ref("payments");
+      await ref.on(
+        "value",
+        async function(snapshot) {
+          dispatch(getPaymentSuccess(snapshot.val()));
+        },
+        function(errorObject) {
+          throw new Error("Error");
+        }
+      );
+    } catch (e) {
+      dispatch(getPaymentFailure("Error in logging in user"));
+    }
+  };
+}
+
+export function getMeaageRequest() {
+  return {
+    type: GET_MESSAGE_REQUEST,
+    status: REQUEST
+  };
+}
+export function getMeaageSuccess(messages) {
+  return {
+    type: GET_MESSAGE_SUCCESS,
+    status: SUCCESS,
+    messages
+  };
+}
+export function getMeaageFailure(error) {
+  return {
+    type: GET_MESSAGE_FAILURE,
+    status: FAILURE,
+    error
+  };
+}
+export function getMeaage() {
+  return async (dispatch, state) => {
+    dispatch(getMeaageRequest());
+    try {
+      let ref = await fire.database().ref("payments");
+      await ref.on(
+        "value",
+        async function(snapshot) {
+          dispatch(getMeaageSuccess(snapshot.val()));
+        },
+        function(errorObject) {
+          throw new Error("Error");
+        }
+      );
+    } catch (e) {
+      dispatch(getMeaageFailure("Error in logging in user"));
     }
   };
 }
--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,7 +1,14 @@
 import React from "react";
 import _ from "lodash";
+import classNames from "classnames";
 import "./css/userDashboard.css";
 export default class UserDashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 1
+    };
+  }
   redirectToAgentInfo(id) {
     this.props.history.push(`/agents/${id}`);
   }
@@ -27,24 +34,58 @@ export default class UserDashboard extends React.Component {
       return this.renderUniqueAgents(item);
     });
   }
+  renderfirst(agents) {
+    return (
+      <div>
+        <div className="UserDashboard-ListHeader">
+          <div className="UserDashboard-ItemAge">Name</div>
+          <div className="UserDashboard-ItemIdNumber">Id No.</div>
+          <div className="UserDashboard-ItemMobile">Mobile</div>
+          <div className="UserDashboard-ItemGender">Gender</div>
+          <div className="UserDashboard-ItemOAmount">occupation</div>
+          <div className="UserDashboard-ItemSelectedOption">location</div>
+        </div>
+        {agents &&
+          agents.map(agent => {
+            return this.renderAgents(agent);
+          })}
+      </div>
+    );
+  }
   render() {
+    let classForFirst = classNames("UserDashboard-tabItem", {
+      "UserDashboard-tabItem-active": this.state.selectedTab === 1
+    });
+    let classForSecond = classNames("UserDashboard-tabItem", {
+      "UserDashboard-tabItem-active": this.state.selectedTab === 2
+    });
+    let classForThird = classNames("UserDashboard-tabItem", {
+      "UserDashboard-tabItem-active": this.state.selectedTab === 3
+    });
     let agents = _.toArray(this.props.agents);
     return (
       <div className="UserDashboard">
-        <div className="UserDashboard-List">
-          <div className="UserDashboard-ListHeader">
-            <div className="UserDashboard-ItemAge">Name</div>
-            <div className="UserDashboard-ItemIdNumber">Id No.</div>
-            <div className="UserDashboard-ItemMobile">Mobile</div>
-            <div className="UserDashboard-ItemGender">Gender</div>
-            <div className="UserDashboard-ItemOAmount">occupation</div>
-            <div className="UserDashboard-ItemSelectedOption">location</div>
+        <div className="UserDashboard-tab">
+          <div
+            className={classForFirst}
+            onClick={() => this.setState({ selectedTab: 1 })}
+          >
+            Agents List
           </div>
-          {agents &&
-            agents.map(agent => {
-              return this.renderAgents(agent);
-            })}
+          <div
+            className={classForSecond}
+            onClick={() => this.setState({ selectedTab: 2 })}
+          >
+            Payment
+          </div>
+          <div
+            className={classForThird}
+            onClick={() => this.setState({ selectedTab: 3 })}
+          >
+            Messages
+          </div>
         </div>
+        <div className="UserDashboard-List">{this.renderfirst(agents)}</div>
       </div>
     );
   }
